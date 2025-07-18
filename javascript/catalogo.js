07dfccaef91489cb102a0d8f704b57361748b0cc
Javi_Rod <inge.javirodriguez@gmail.com>
2025-07-17 18:30:20 -0600

    Modificacion de tarjeta de producto No.1

--- javascript/catalogo.js
+++ javascript/catalogo.js
@@ -1,24 +1,24 @@
 export const productos = [
-    { 
+    {
         id: 1,
-        producto: 'Player 1',
-        imagenes:[
-            "https://i.postimg.cc/qRTyLtGh/Playera1.png",
-        "https://i.postimg.cc/yd12kybv/476597354-938049468439041-8383495441410287182-n.jpg",
-        "https://i.postimg.cc/B6mkD1Ws/480730965-944112604499394-1496808190388782122-n.jpg",
+        producto: 'Sudadera esqueleto',
+        imagenes: [
+            "https://i.postimg.cc/1zJ2cBPk/sudadera-esqueleto-cafe-1.jpg",
+            "https://i.postimg.cc/qBPJ3J18/sudadera-esqueleto-cafe-2.jpg",
+            "https://i.postimg.cc/8PL1d2Yx/sudadera-esqueleto-cafe-3.jpg",
         ],
-        descripcion: "Playera buena calidad",
+        descripcion: "Sudadera color negra con diseño de esqueleto humano con técnica de decoloración de tela, 100% algodón. ¡Muy chida!",
         tallas: ["S", "M", "L"],
-        precio: 1000,
-        categoria: "playeras"
+        precio: 1200,
+        categoria: "Chamarras"
     },
     {
         id: 2,
         producto: 'Player2',
-        imagenes:[
-        "https://i.postimg.cc/4y4cDrqX/Playera2.png",
-        "https://i.postimg.cc/yd12kybv/476597354-938049468439041-8383495441410287182-n.jpg",
-        "https://i.postimg.cc/B6mkD1Ws/480730965-944112604499394-1496808190388782122-n.jpg",
+        imagenes: [
+            "https://i.postimg.cc/4y4cDrqX/Playera2.png",
+            "https://i.postimg.cc/yd12kybv/476597354-938049468439041-8383495441410287182-n.jpg",
+            "https://i.postimg.cc/B6mkD1Ws/480730965-944112604499394-1496808190388782122-n.jpg",
         ],
         descripcion: "Playera buena calidad",
         tallas: ["S", "M", "L"],
@@ -28,23 +28,23 @@ export const productos = [
     {
         id: 3,
         producto: 'Playera "aventuras de una mosca"',
-        imagenes:[
-        "https://i.postimg.cc/1X6qWB7n/2.jpg",
-        "https://i.postimg.cc/SsH973db/2-3.jpg",
-        "https://i.postimg.cc/L4ZL6449/2-1.jpg",
+        imagenes: [
+            "https://i.postimg.cc/1X6qWB7n/2.jpg",
+            "https://i.postimg.cc/SsH973db/2-3.jpg",
+            "https://i.postimg.cc/L4ZL6449/2-1.jpg",
         ],
         descripcion: "Playera 100% algodón, manga corta",
         tallas: ["S", "M", "L"],
         precio: 300,
         categoria: "playeras"
     },
-    { 
+    {
         id: 4,
         producto: 'Jogger Cargo Negro',
-        imagenes:[
+        imagenes: [
             "https://i.postimg.cc/BbvYf0vm/11.jpg",
-        "https://i.postimg.cc/5tMsNv0Y/11-1.jpg",
-        "https://i.postimg.cc/4yM5CHq3/11-2.jpg",
+            "https://i.postimg.cc/5tMsNv0Y/11-1.jpg",
+            "https://i.postimg.cc/4yM5CHq3/11-2.jpg",
         ],
         descripcion: "Pantalon estilo jogger buena calidad",
         tallas: ["S", "M", "L"],
@@ -54,10 +54,10 @@ export const productos = [
     {
         id: 5,
         producto: 'Pantalon Militar',
-        imagenes:[
-        "https://i.postimg.cc/Nj7FDDXw/16-pantalon-negro.jpg",
-        "https://i.postimg.cc/28WVbr8y/16-2-pantalon-negro.jpg",
-        "https://i.postimg.cc/8zVsL9jH/16-3-pantalon-negro.jpg",
+        imagenes: [
+            "https://i.postimg.cc/Nj7FDDXw/16-pantalon-negro.jpg",
+            "https://i.postimg.cc/28WVbr8y/16-2-pantalon-negro.jpg",
+            "https://i.postimg.cc/8zVsL9jH/16-3-pantalon-negro.jpg",
         ],
         descripcion: "Pantalon estampado militar",
         tallas: ["S", "M", "L"],
@@ -67,23 +67,23 @@ export const productos = [
     {
         id: 6,
         producto: 'The King Tiger joggers in Black',
-        imagenes:[
-        "https://i.postimg.cc/7CKKJBK1/15.jpg",
-        "https://i.postimg.cc/TLhCVrsz/15-1.jpg",
-        "https://i.postimg.cc/ftF5BVnJ/15-2.jpg",
+        imagenes: [
+            "https://i.postimg.cc/7CKKJBK1/15.jpg",
+            "https://i.postimg.cc/TLhCVrsz/15-1.jpg",
+            "https://i.postimg.cc/ftF5BVnJ/15-2.jpg",
         ],
         descripcion: "Joggers negros con bolsillos laterales",
         tallas: ["S", "M", "L"],
         precio: 1200,
         categoria: "Pantalones"
     },
-    { 
+    {
         id: 7,
         producto: 'Playera negra Billie Ellish',
-        imagenes:[
-        "https://i.postimg.cc/JnL8YpLs/billie-elish-3.jpg",
-        "https://i.postimg.cc/K8CxBZpr/billie-elish-1.jpg",
-        "https://i.postimg.cc/DzsFcttV/billie-elish-2.jpg",
+        imagenes: [
+            "https://i.postimg.cc/JnL8YpLs/billie-elish-3.jpg",
+            "https://i.postimg.cc/K8CxBZpr/billie-elish-1.jpg",
+            "https://i.postimg.cc/DzsFcttV/billie-elish-2.jpg",
         ],
         descripcion: "Playera buena calidad, manga corta",
         tallas: ["S", "M", "L"],
@@ -93,10 +93,10 @@ export const productos = [
     {
         id: 8,
         producto: 'Player 2',
-        imagenes:[
-        "https://i.postimg.cc/4y4cDrqX/Playera2.png",
-        "https://i.postimg.cc/yd12kybv/476597354-938049468439041-8383495441410287182-n.jpg",
-        "https://i.postimg.cc/B6mkD1Ws/480730965-944112604499394-1496808190388782122-n.jpg",
+        imagenes: [
+            "https://i.postimg.cc/4y4cDrqX/Playera2.png",
+            "https://i.postimg.cc/yd12kybv/476597354-938049468439041-8383495441410287182-n.jpg",
+            "https://i.postimg.cc/B6mkD1Ws/480730965-944112604499394-1496808190388782122-n.jpg",
         ],
         descripcion: "Playera buena calidad",
         tallas: ["S", "M", "L"],
@@ -106,10 +106,10 @@ export const productos = [
     {
         id: 9,
         producto: 'Sudadera esqueleto',
-        imagenes:[
-        "https://i.postimg.cc/5yq0Q7Fw/sudadera-esqueleto-uva-1.jpg",
-        "https://i.postimg.cc/qqdqRVgJ/sudadera-esqueleto-uva-2.jpg",
-        "https://i.postimg.cc/Ss9jkNS1/sudadera-esqueleto-uva-3.jpg",
+        imagenes: [
+            "https://i.postimg.cc/5yq0Q7Fw/sudadera-esqueleto-uva-1.jpg",
+            "https://i.postimg.cc/qqdqRVgJ/sudadera-esqueleto-uva-2.jpg",
+            "https://i.postimg.cc/Ss9jkNS1/sudadera-esqueleto-uva-3.jpg",
         ],
         descripcion: "Sudadera color uva con diseño de esqueleto humano con técnica de decoloración de tela, 100% algodón. ¡Muy chida!",
         tallas: ["S", "M", "L"],
@@ -119,10 +119,10 @@ export const productos = [
     {
         id: 10,
         producto: 'Sudadera van Gogh',
-        imagenes:[
-        "https://i.postimg.cc/sxfmb8yX/9-2.jpg",
-        "https://i.postimg.cc/W3wSWVBC/9.jpg",
-        "https://i.postimg.cc/NFNDt4fx/9-1.jpg",
+        imagenes: [
+            "https://i.postimg.cc/sxfmb8yX/9-2.jpg",
+            "https://i.postimg.cc/W3wSWVBC/9.jpg",
+            "https://i.postimg.cc/NFNDt4fx/9-1.jpg",
         ],
         descripcion: "Sudadera cómoda y fresca color rojo con capucha",
         tallas: ["S", "M", "L"],
