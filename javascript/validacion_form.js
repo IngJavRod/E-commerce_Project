4759ac8502eb9c347b84208fa71a6121d76c4bd3
JonathanDG <32504527+JonathanDG@users.noreply.github.com>
2025-08-13 09:18:10 -0600

    update en form de products

--- javascript/validacion_form.js
+++ javascript/validacion_form.js
@@ -2,24 +2,36 @@ document.getElementById('productoForm').addEventListener('submit', function (e) 
     e.preventDefault(); // Evita que se envíe el formulario
 
     const nombre = document.getElementById('nombreProducto').value.trim();
-    const tallas = document.getElementById('tallasProducto').value.trim();
+    //const tallas = document.getElementById('tallasProducto').value.trim();
     const categoriasCheckbox = document.querySelectorAll('input[name="categoria"]');
     const precio = document.getElementById('precioProducto').value.trim();
     const descripcion = document.getElementById('descripcionProducto').value.trim();
     const url = document.getElementById('imagenesProducto').value.trim();
+    const tallaCH = document.getElementById('tallaCH').value.trim();
+    const tallaM = document.getElementById('tallaM').value.trim();
+    const tallaG = document.getElementById('tallaG').value.trim();
+    const unitalla = document.getElementById('unitalla').value.trim();
 
     const categoriasSeleccionadas = Array.from(categoriasCheckbox)
         .filter(checkbox => checkbox.checked)
         .map(checkbox => checkbox.value);
 
+    
     let errores = [];
 
     if (categoriasSeleccionadas.length === 0) {
         errores.push("Debes seleccionar al menos una categoría.");
     }
     if (nombre === '') errores.push("El nombre es obligatorio.");
-    if (tallas === '') errores.push("Las tallas son obligatorias.");
-    if (precio === '' || isNaN(precio) || Number(precio) <= 0) errores.push("El precio debe ser un número válido mayor que 0.");
+    //if (tallas === '') errores.push("Las tallas son obligatorias.");
+    
+    
+    if (Number(tallaCH) <0) errores.push("Stock CH debe ser mayor o igual a 0.")
+    if (Number(tallaM) <0) errores.push("Stock M debe ser mayor o igual a 0.")
+    if (tallaG <0) errores.push("Stock G debe ser mayor o igual a 0.")
+    if (unitalla <0) errores.push("Stock Unitalla debe ser mayor o igual a 0.")
+
+    //if (precio === '' || isNaN(precio) || Number(precio) <= 0) errores.push("El precio debe ser un número válido mayor que 0.");
     if (descripcion.length < 10) errores.push("La descripción debe tener al menos 10 caracteres.");
     if (!url.match(/^https?:\/\/.+\.(jpg|jpeg|png|gif|webp)$/i)) errores.push("La URL debe ser válida y apuntar a una imagen.");
 
@@ -27,11 +39,41 @@ document.getElementById('productoForm').addEventListener('submit', function (e) 
 
     if (errores.length === 0) {
         // Objeto JSON
+
+        let ch, m, g, ut;
+        if (tallaCH === '') {
+          ch = 0;
+        }else{
+          ch = Number(tallaCH)
+        }
+
+        if (tallaM === '') {
+          m = 0;
+        }else{
+          m = Number(tallaM)
+        }
+
+        if (tallaG === '') {
+          g = 0;
+        }else{
+          g = Number(tallaG)
+        }
+
+        if (unitalla === '') {
+          ut = 0;
+        }else{
+          ut = Number(unitalla)
+        }
+
         const producto = {
             nombre,
             imagenes: url.split(',').map(img => img.trim()),
             descripcion,
-            tallas: tallas.split(',').map(talla => talla.trim()),
+            tallaCH: Number(ch),
+            tallaM: Number(m),
+            tallaG: Number(g),
+            unitalla: Number(ut),
+            // tallas.split(',').map(talla => talla.trim()),
             precio: Number(precio),
             categorias: categoriasSeleccionadas
         };
