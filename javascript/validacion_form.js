3e1eac2b19df0ad2b4c53b4dc6e2982df833b815
Alejandro <asotomiguel11@gmail.com>
2025-08-13 18:27:33 -0600

    Backend productos-usuarios

--- javascript/validacion_form.js
+++ javascript/validacion_form.js
@@ -1,39 +1,40 @@
 document.getElementById('productoForm').addEventListener('submit', async function (e) {
     e.preventDefault(); // Evita que se envíe el formulario
 
-    const nombre = document.getElementById('nombreProducto').value.trim();
+    const nombreInput = document.getElementById('nombreProducto').value.trim();
     //const tallas = document.getElementById('tallasProducto').value.trim();
-    const categoriasCheckbox = document.querySelectorAll('input[name="categoria"]');
-    const precio = document.getElementById('precioProducto').value.trim();
-    const descripcion = document.getElementById('descripcionProducto').value.trim();
-    const archivosImagenes = document.getElementById('imagenArchivo').files;
-    const tallaCH = document.getElementById('tallaCH').value.trim();
-    const tallaM = document.getElementById('tallaM').value.trim();
-    const tallaG = document.getElementById('tallaG').value.trim();
-    const unitalla = document.getElementById('unitalla').value.trim();
-
-    const categoriasSeleccionadas = Array.from(categoriasCheckbox)
+    const categoriasCheckboxInput = document.querySelectorAll('input[name="categoria"]');
+    const precioInput = document.getElementById('precioProducto').value.trim();
+    const descripcionInput = document.getElementById('descripcionProducto').value.trim();
+    const archivosImagenesInput = document.getElementById('imagenArchivo').files;
+    const tallaCHInput = document.getElementById('tallaCH').value.trim();
+    const tallaMInput = document.getElementById('tallaM').value.trim();
+    const tallaGInput = document.getElementById('tallaG').value.trim();
+    const unitallaInput = document.getElementById('unitalla').value.trim();
+    const descuentoInput = 10;
+    const categoriasSeleccionadasInput = Array.from(categoriasCheckboxInput)
         .filter(checkbox => checkbox.checked)
         .map(checkbox => checkbox.value);
 
-    
+    const API_URL = `http://localhost:8080/db/v1/thekingtiger/agregar-producto`;
+
     let errores = [];
 
-    if (categoriasSeleccionadas.length === 0) {
+    if (categoriasSeleccionadasInput.length === 0 || categoriasSeleccionadasInput.length > 1) {
         errores.push("Debes seleccionar al menos una categoría.");
     }
-    if (nombre === '') errores.push("El nombre es obligatorio.");
+    if (nombreInput === '') errores.push("El nombre es obligatorio.");
     //if (tallas === '') errores.push("Las tallas son obligatorias.");
     
     
-    if (Number(tallaCH) <0) errores.push("Stock CH debe ser mayor o igual a 0.")
-    if (Number(tallaM) <0) errores.push("Stock M debe ser mayor o igual a 0.")
-    if (tallaG <0) errores.push("Stock G debe ser mayor o igual a 0.")
-    if (unitalla <0) errores.push("Stock Unitalla debe ser mayor o igual a 0.")
+    if (Number(tallaCHInput) <0) errores.push("Stock CH debe ser mayor o igual a 0.")
+    if (Number(tallaMInput) <0) errores.push("Stock M debe ser mayor o igual a 0.")
+    if (tallaGInput <0) errores.push("Stock G debe ser mayor o igual a 0.")
+    if (unitallaInput <0) errores.push("Stock Unitalla debe ser mayor o igual a 0.")
 
     //if (precio === '' || isNaN(precio) || Number(precio) <= 0) errores.push("El precio debe ser un número válido mayor que 0.");
-    if (descripcion.length < 10) errores.push("La descripción debe tener al menos 10 caracteres.");
-    if (archivosImagenes.length === 0) errores.push("Debes subir al menos una imagen.");
+    if (descripcionInput.length < 10) errores.push("La descripción debe tener al menos 10 caracteres.");
+    if (archivosImagenesInput.length === 0) errores.push("Debes subir al menos una imagen.");
 
     mostrarErrores(errores);
 
@@ -41,52 +42,89 @@ document.getElementById('productoForm').addEventListener('submit', async functio
         // Objeto JSON
 
         let ch, m, g, ut;
-        if (tallaCH === '') {
+        if (tallaCHInput === '') {
           ch = 0;
         }else{
-          ch = Number(tallaCH)
+          ch = Number(tallaCHInput)
         }
 
-        if (tallaM === '') {
+        if (tallaMInput === '') {
           m = 0;
         }else{
-          m = Number(tallaM)
+          m = Number(tallaMInput)
         }
 
-        if (tallaG === '') {
+        if (tallaGInput === '') {
           g = 0;
         }else{
-          g = Number(tallaG)
+          g = Number(tallaGInput)
         }
 
-        if (unitalla === '') {
+        if (unitallaInput === '') {
           ut = 0;
         }else{
-          ut = Number(unitalla)
+          ut = Number(unitallaInput)
         }
         
         try{
           // Subir imágenes a Cloudinary
-          const urlsImagenes = await subirVariasImagenesCloudinary(archivosImagenes);
-          const producto = {
-            nombre,
-            imagenes: urlsImagenes,
-            descripcion,
-            tallaCH: Number(ch),
-            tallaM: Number(m),
-            tallaG: Number(g),
+          const urlsImagenes = await subirVariasImagenesCloudinary(archivosImagenesInput);
+
+          // Unir las URLs en un solo string separado por comas
+          const imagenesString = urlsImagenes.join(', ');
+
+          const categoria = categoriasSeleccionadasInput[0];
+
+          const productos = {
+            nomProd: nombreInput,
+            imagenes: imagenesString,
+            descripcion: descripcionInput,
+            chica: Number(ch),
+            mediana: Number(m),
+            grande: Number(g),
             unitalla: Number(ut),
-            // tallas.split(',').map(talla => talla.trim()),
-            precio: Number(precio),
-            categorias: categoriasSeleccionadas
+            precio: parseFloat(precioInput),
+            categoria: categoria,
+            descuento: descuentoInput
           };
-          // Mostrar en consola 
-          console.log("Producto creado:", JSON.stringify(producto, null, 2));
-          //Mostrar mensaje en el modal
-          document.getElementById("modalMessage").innerText = "Producto guardado exitosamente.";
-          new bootstrap.Modal(document.getElementById("responseModal")).show()
-          // Opcional: limpiar el formulario
-          document.getElementById("productoForm").reset();
+          console.log("Producto creado:", JSON.stringify(productos, null, 2));
+          try{
+            const res = await fetch(API_URL, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(productos)
+            });
+
+            if (!res.ok){
+              throw new Error(`Error en la peticion: ${res.status}`);
+            }
+
+            const savedProduct = await res.json();
+            console.log('Producto registrado:', savedProduct);
+
+            //Mostrar mensaje en el modal
+            document.getElementById("modalMessage").innerText = "Producto guardado exitosamente.";
+            new bootstrap.Modal(document.getElementById("responseModal")).show()
+            
+            //Limpiar el formulario
+            document.getElementById("productoForm").reset();
+            //Reinciar variables
+            nombreInput.value = '';
+            descripcionInput.value = '';
+            tallaCHInput.value = '';
+            tallaMInput.value = '';
+            tallaGInput.value = '';
+            unitallaInput.value = '';
+            precioInput.value = '';
+            categoria.value = '';
+
+          }catch(err){
+            console.error(err);
+            alert('Hubo un error al registrar el producto.');
+          }
+          
         } catch (error) {
           mostrarErrores(["Error al subir imágenes: " + error.message]);
         }
